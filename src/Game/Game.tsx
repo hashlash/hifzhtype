d4daa4f8b9ef2add1b0c8cbc7f4f8300d1660bee
hashlash <23237214+hashlash@users.noreply.github.com>
2026-05-09 20:58:25 +0700

    Complete Surah Al-Fatihah ayahs in Game.tsx (#3)

--- src/Game/Game.tsx
+++ src/Game/Game.tsx
@@ -1,30 +1,14 @@
 import AyahDisplay from './AyahDisplay';
 import Input from './Input';
+import { fatihah } from '../data/surah/1';
 
 export default function Game() {
-  const ayahs = [
-    {
-      words: [
-        { text: 'بِسْمِ' },
-        { text: 'اللَّهِ' },
-        { text: 'الرَّحْمَٰنِ' },
-        { text: 'الرَّحِيمِ' },
-      ],
-    },
-    {
-      words: [
-        { text: 'الْحَمْدُ' },
-        { text: 'لِلَّهِ' },
-        { text: 'رَبِّ' },
-        { text: 'الْعَالَمِينَ' },
-      ],
-    },
-    {
-      words: [{ text: 'الرَّحْمَٰنِ' }, { text: 'الرَّحِيمِ' }],
-    },
-  ];
+  const ayahs = fatihah.ayahs;
   return (
-    <div>
+    <div dir="rtl" lang="ar">
+      <h1>
+        {fatihah.number}. {fatihah.transliteration} ({fatihah.name})
+      </h1>
       <AyahDisplay ayahs={ayahs}></AyahDisplay>
       <br />
       <Input></Input>
